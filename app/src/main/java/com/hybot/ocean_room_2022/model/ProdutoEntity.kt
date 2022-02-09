003a1a3f340ee2a3a363c1eee2e96906067b2c04
Helder Thury <exp.helder.0@gmail.com>
2022-02-09 09:22:27 -0400

    Importando room

---
+++ app/src/main/java/com/hybot/ocean_room_2022/model/ProdutoEntity.kt
@@ -0,0 +1,8 @@
+package com.hybot.ocean_room_2022.model
+
+import androidx.room.Entity
+
+@Entity
+data class ProdutoEntity(
+
+)
--- app/src/main/java/com/hybot/ocean_room_2022/view/MainActivity.kt
+++ app/src/main/java/com/hybot/ocean_room_2022/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hybot.ocean_room_2022
+package com.hybot.ocean_room_2022.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hybot.ocean_room_2022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
